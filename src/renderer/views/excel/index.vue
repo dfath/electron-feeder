c2cc37bc15875a4d119fa9dbe4276e4750ae6d07
unknown <>
2019-07-04 03:57:20 +0700

    menambahkan UI

--- src/renderer/views/excel/index.vue
+++ src/renderer/views/excel/index.vue
@@ -2,43 +2,74 @@
   <div class="app-container">
     <h1>Upload File Excel</h1>
     <el-row  type="flex" justify="center" >
-  <el-col :span="24">
-    <div class="grid-content bg-purple">
-      <el-row  type="flex" justify="end" >
-        <el-col :span="6">
-          <div>
-             <el-button type="info">Download contoh file Excel</el-button>
-          </div>
-        </el-col>
-        <el-col :span="6">
-          <div>
-             <el-button type="success">Kirim ke Feeder PDDikti</el-button>
-          </div>
-        </el-col>
-      </el-row>
-   
-   
-    <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
-    
-    </div></el-col>
-</el-row>
+      <el-col :span="24">
+        <div class="grid-content">
+          <el-row  type="flex" justify="end" >
+            <el-col :span="6">
+              <div>
+                <el-button type="info">Download contoh file Excel</el-button>
+              </div>
+            </el-col>
+            <el-col :span="6">
+              <div>
+                <el-popover
+                  placement="top"
+                  width="160"
+                  v-model="visible">
+                  <p>Apakah Anda yakin untuk mengirimkan data ini?</p>
+                  <div style="text-align: right; margin: 0">
+                    <el-button size="mini" type="text" @click="visible = false">Tidak</el-button>
+                    <el-button type="primary" size="mini" @click="visible = false">Ya</el-button>
+                  </div>
+                  <el-button type="success" slot="reference">Kirim ke Feeder PDDikti</el-button>
+                </el-popover>
+              </div>
+            </el-col>
+          </el-row>
+      
+      
+        <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
+        
+        </div></el-col>
+    </el-row>
     
     <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
       <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
     </el-table>
+    <div class="components-container">
+
+
+      <!-- you can add element-ui's tooltip -->
+      <el-tooltip placement="top" content="tooltip">
+        <back-to-top :custom-style="myBackToTopStyle" :visibility-height="300" :back-position="50" transition-name="fade" />
+      </el-tooltip>
+    </div>
   </div>
 </template>
 
 
 <script>
 import UploadExcelComponent from '@/components/UploadExcel/index.vue'
+import BackToTop from '@/components/BackToTop'
+
 export default {
   name: 'UploadExcel',
-  components: { UploadExcelComponent },
+  components: { UploadExcelComponent, BackToTop },
   data() {
     return {
       tableData: [],
-      tableHeader: []
+      tableHeader: [],
+      visible: false,
+
+      myBackToTopStyle: {
+        right: '50px',
+        bottom: '50px',
+        width: '40px',
+        height: '40px',
+        'border-radius': '4px',
+        'line-height': '45px', // 请保持与高度一致以垂直居中 Please keep consistent with height to center vertically
+        background: '#e7eaf1'// 按钮的背景颜色 The background color of the button
+      }
     }
   },
   methods: {
@@ -94,5 +125,8 @@ export default {
   .row-bg {
     padding: 10px 0;
     background-color: #f9fafc;
+  }
+  .placeholder-container div {
+  margin: 10px;
   }
 </style>
---
+++ src/renderer/views/profil/index.vue
@@ -0,0 +1,235 @@
+<template>
+  <div class="app-container">
+    <div>
+      <el-table border :data="tableProfil">
+        <el-table-column min-width="50" prop="name" label=""></el-table-column>
+        <el-table-column min-width="150" prop="isi">
+        </el-table-column>
+      </el-table>
+    </div>
+    <br>
+    <br>
+    <div>
+      <el-tabs type="border-card">
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-location-outline"></i> Alamat</span>
+          Alamat
+          <el-table border :data="tableAlamat">
+            <el-table-column min-width="50" prop="name"></el-table-column>
+            <el-table-column min-width="150" prop="isi">
+            </el-table-column>
+          </el-table>
+
+        </el-tab-pane>
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-info"></i> Informasi PT</span>
+          Informasi PT
+
+          <el-table border :data="tableInfoPT">
+            <el-table-column min-width="50" prop="name"></el-table-column>
+            <el-table-column min-width="150" prop="isi">
+            </el-table-column>
+          </el-table>
+
+
+        </el-tab-pane>
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-document"></i> Akta Pendirian</span>
+          Akta Pendirian
+        </el-tab-pane>
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-news"></i> Program Studi</span>
+          Program Studi
+          <el-table border :data="tableData">
+            <el-table-column min-width="50" type="index"></el-table-column>
+            <el-table-column min-width="150" prop="name"
+                            label="Name">
+            </el-table-column>
+            <el-table-column min-width="200" prop="job"
+                            label="Job Position">
+            </el-table-column>
+            <el-table-column min-width="150" prop="salary"
+                            label="Salary">
+            </el-table-column>
+            <el-table-column min-width="150"
+                            header-align="right"
+                            label="Actions">
+              <div slot-scope="{row}" class="text-right">
+                <el-tooltip content="Info"
+                            :open-delay="300"
+                            placement="top">
+                  <p-button type="info" size="sm" icon>
+                    <i class="nc-icon nc-alert-circle-i"></i>
+                  </p-button>
+                </el-tooltip>
+
+
+                <el-tooltip content="Settings"
+                            :open-delay="300"
+                            placement="top">
+                  <p-button type="success" size="sm" icon>
+                    <i class="nc-icon nc-settings"></i>
+                  </p-button>
+                </el-tooltip>
+
+                <el-tooltip content="Delete"
+                            :open-delay="300"
+                            placement="top">
+                  <p-button type="danger" size="sm" icon>
+                    <i class="nc-icon nc-simple-delete"></i>
+                  </p-button>
+                </el-tooltip>
+
+              </div>
+            </el-table-column>
+          </el-table>
+
+        </el-tab-pane>
+        <el-tab-pane>
+          <span slot="label"><i class="el-icon-date"></i> Periode Pelaporan Pengajuan</span>
+          Periode Pelaporan Pengajuan
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getList } from '@/api/table'
+
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true,
+      activeName: 'first',
+
+      tableData: [{
+        id: 1,
+        name: 'Dakota Rice',
+        job: 'Develop',
+        salary: '$36.738',
+        country: 'Niger',
+        city: 'Oud-Turnhout'
+      },
+      {
+        id: 2,
+        name: 'Minerva Hooper',
+        job: 'Marketing',
+        salary: '$23,789',
+        country: 'Curaçao',
+        city: 'Sinaai-Waas'
+      }],
+      tableProfil: [{
+        name: 'Kode PT',
+        isi: '043115'
+      },
+      {
+        name: 'Nama PT',
+        isi: 'Sekolah Tinggi Farmasi Bandung'
+      },
+      {
+        name: 'Telepon',
+        isi: '7830760'
+      },
+      {
+        name: 'Faxmile',
+        isi: '7830760'
+      },
+      {
+        name: 'Email',
+        isi: 'contacct@stfb.ac.id'
+      },
+      {
+        name: 'Website',
+        isi: 'www.stfb.ac.id'
+      }],
+
+      tableInfoPT: [{
+        name: 'Bank',
+        isi: 'Bank BNI'
+      },
+      {
+        name: 'Unit Cabang',
+        isi: 'Cabang Bandung'
+      },
+      {
+        name: 'Nomor Rekening',
+        isi: '700000'
+      },
+      {
+        name: 'MBS',
+        isi: '0'
+      },
+      {
+        name: 'Luas Tanah Milik',
+        isi: '0'
+      },
+      {
+        name: 'Luas Tanah Milik',
+        isi: '0'
+      }],
+      tableAlamat: [{
+        name: 'Jalan',
+        isi: 'Jalan Soekarno Hatta No 754, Cibiru Bandung'
+      },
+      {
+        name: 'Dusun',
+        isi: ''
+      },
+      {
+        name: 'RT/RW',
+        isi: ''
+      },
+      {
+        name: 'Kelurahan',
+        isi: ''
+      },
+      {
+        name: 'Kodepos',
+        isi: '40161'
+      },
+      {
+        name: 'Kecamatan',
+        isi: 'Kota Bandung'
+      },
+      {
+        name: 'Lintang Bujur',
+        isi: 'Bujur'
+      }]
+
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList(this.listQuery).then(response => {
+        this.list = null
+        this.listLoading = false
+      })
+    },
+    handleClick(tab, event) {
+      console.log(tab, event)
+    }
+  }
+}
+</script>
